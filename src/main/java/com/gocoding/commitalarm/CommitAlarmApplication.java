e0975ea2aedd99596321c9f55c9c168609fc3f5a
go-coding1 <rhtjrwnszyzy@gmail.com>
2024-01-06 13:59:11 +0900

    FirstCommit

---
+++ src/main/java/com/gocoding/commitalarm/CommitAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.gocoding.commitalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitAlarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gocoding/commitalarm/CommitAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gocoding.commitalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
